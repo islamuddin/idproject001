ca3b0f1edaf54d7c4f1f08fa535f483ece75d8a4
islamuddin <uddinislam46@gmail.com>
2020-06-02 23:17:15 +0500

    updated build files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1><center>ID KHAN</center></h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload....
         </p>
         
         <a
